a51da5f1b33b74b33812b1eb85df0713078abaf2
Connor Hansen <chansen13@georgefox.edu>
2021-08-31 16:25:55 -0700

    add full tests, it needs some forced variables as a workaround to random dice rolls.

--- __tests__/pig-dice.test.js
+++ __tests__/pig-dice.test.js
@@ -3,32 +3,49 @@ import { Player } from '../src/pig-dice.js';
 
 
 describe('Player', () => {
-  
-  // let reusableTriangle;
-  // test('should show how beforeEach() works', () => {
-  //   console.log(reusableTriangle);
-  // });
 
   
   test('Should create an instance of the player, with scores of 0 and turnscore of 0', () => {
-    const player = new Player("player1", 0, 10);
+    const player = new Player("player1", 0, 10, 1);
     expect(player.name).toEqual("player1");
     expect(player.score).toEqual(0);
     expect(player.turnScore).toEqual(10);
   });
   
   test('Player rolling dice should be between 1 & 6', () => {
-    const player = new Player("player1", 0, 0);
+    const player = new Player("player1", 0, 0, 1);
     player.rollDice();
     expect(player.turnScore).toBeLessThanOrEqual(6);
-    expect(player.turnScore).toBeGreaterThanOrEqual(1);
+    expect(player.turnScore).toBeGreaterThanOrEqual(0);
+    if (player.turnScore === 0) {
+      expect(player.turnCounter).toEqual(2);
+    }
+    // expect(player.turnCounter).toBeLessThanOrEqual(2);
+    // expect(player.turnCounter).toBeGreaterThanOrEqual(1);
   });
 
+
+  test('on roll of 1 player turn counter should change', () => {
+    const player = new Player("player2", 0, 0, 2);
+    player.rollDice();
+    if (player.turnScore === 0) {
+      expect(player.turnCounter).toEqual(1);
+    }
+  });
+
+
   test('Player hold should change turn and save the score', () => {
-    const player = new Player("player1", 0, 0);
+    const player = new Player("player1", 0, 0, 1);
     player.hold();
     expect(player.turnScore).toEqual(0);
     expect(player.score).toEqual(player.turnScore);
+    expect(player.turnCounter).toEqual(2);
+  });
+
+  test('Player2 hold should change turn and save the score', () => {
+    const player = new Player("player2", 0, 0, 2);
+    player.hold();
+    expect(player.turnCounter).toEqual(1);
   });
 
 });
--- src/pig-dice.js
+++ src/pig-dice.js
@@ -1,20 +1,24 @@
-var turnCounter = 1;
+// var turnCounter = 1;
 
-export function Player(name, score, turnScore) {
+export function Player(name, score, turnScore, turnCounter) {
   this.name = name;
   this.score = score;
   this.turnScore = turnScore;
+  this.turnCounter = turnCounter;
 }
 
 Player.prototype.rollDice = function () {
   let tempScore = Math.floor(Math.random() * 6 + 1);
+  // Test with forced roll of 1
+  // tempScore = 1;
+  // ***************************
   console.log("Roll score: " + tempScore);
   if (tempScore === 1) {
     this.turnScore = 0;
-    if (turnCounter === 1) {
-      turnCounter = 2;
-    } else if (turnCounter === 2) {
-      turnCounter = 1;
+    if (this.turnCounter === 1) {
+      this.turnCounter = 2;
+    } else if (this.turnCounter === 2) {
+      this.turnCounter = 1;
     }
   } else {
     this.turnScore += tempScore;
@@ -25,9 +29,9 @@ Player.prototype.rollDice = function () {
 Player.prototype.hold = function () {
   this.score += this.turnScore;
   this.turnScore = 0;
-  if (turnCounter === 1) {
-    turnCounter = 2;
-  } else if (turnCounter === 2) {
-    turnCounter = 1;
+  if (this.turnCounter === 1) {
+    this.turnCounter = 2;
+  } else if (this.turnCounter === 2) {
+    this.turnCounter = 1;
   }
 }
