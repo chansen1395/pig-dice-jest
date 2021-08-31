5d8c4e32cd086fb82ee41ec6d012a37d8e039807
Connor Hansen <chansen13@georgefox.edu>
2021-08-31 13:22:08 -0700

    add dev environment, first test passes.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,58 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import Player from './pig-dice.js';
 
-$(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3);
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
-    console.log("test" + length1);
-  });
+player1 = new Player("player1", 0, 0);
+player2 = new Player("player2", 0, 0);
+var turnCounter = 1;
+
+$(document).ready(function () {
+  $("#player-1").addClass("player-1");
+  $("#playerRoll").click(function () {
+    console.log("Turn counter: " + turnCounter);
+    if (turnCounter === 1) {
+      $("#player-1").addClass("player-1");
+      $("#player-2").removeClass("player-1");
+      player1.rollDice();
+    } else if (turnCounter === 2) {
+      $("#player-2").addClass("player-1");
+$("#player-1").removeClass("player-1");
+player2.rollDice();
+}
+$("#player1Score").html(player1.score);
+$("#player1TurnScore").html(player1.turnScore);
+$("#player2Score").html(player2.score);
+$("#player2TurnScore").html(player2.turnScore);
+});
+
+$("#playerHold").click(function () {
+console.log("Turn counter: " + turnCounter);
+if (turnCounter === 1){
+player1.hold();
+$("#player-1").addClass("player-1");
+$("#player-2").removeClass("player-1");
+$("#player1TurnScore").html(0);
+if (player1.score >= 20) {
+$("#playerHold,#playerRoll").hide();
+$("#player1Win").show();
+}
+console.log("P1 Score: " + player1.score);
+} else if (turnCounter === 2) {
+player2.hold();
+$("#player-2").addClass("player-1");
+$("#player-1").removeClass("player-1");
+$("#player2TurnScore").html(0);
+if (player2.score >= 20) {
+$("#playerHold,#playerRoll").hide();
+$("#player2Win").show();
+}
+console.log("P2 Score: " + player2.score);
+}
+$("#player1Score").html(player1.score);
+$("#player2Score").html(player2.score);
+$(".btn-warning").click(function () {
+location.reload();
+});
+});
 });
---
+++ src/pig-dice.js
@@ -0,0 +1,32 @@
+
+export function Player(name, score, turnScore) {
+  this.name = name;
+  this.score = score;
+  this.turnScore = turnScore;
+}
+
+Player.prototype.rollDice = function () {
+  tempScore = Math.floor(Math.random() * 6 + 1);
+  console.log("Roll score: " + tempScore);
+  if (tempScore === 1) {
+    this.turnScore = 0;
+    if (turnCounter === 1) {
+      turnCounter = 2;
+    } else if (turnCounter === 2) {
+      turnCounter = 1;
+    }
+  } else {
+    this.turnScore += tempScore;
+  }
+}
+
+
+Player.prototype.hold = function () {
+  this.score += this.turnScore;
+  this.turnScore = 0;
+  if (turnCounter === 1) {
+    turnCounter = 2;
+  } else if (turnCounter === 2) {
+    turnCounter = 1;
+  }
+}
